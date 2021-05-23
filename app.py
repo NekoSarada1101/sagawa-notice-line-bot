1de5f31dfc53bda7e5c4b7750181b45df51fbf26
NekoSarada1101 <ryoyukikazu11@gmail.com>
2021-05-23 11:50:04 +0900

    feat: 内容を通常の文字列にして表示
    #5

--- app.py
+++ app.py
@@ -15,17 +15,14 @@ def hello_world():
 @app.route('/line', methods=['POST'])
 def push_notice():
     post_data = request.get_data()  # type: str
-    print(post_data)
-    text = request.form.get('text')
-    print(text)
+    print(post_data.encode().decode('unicode-escape'))
 
     line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
     try:
-        line_bot_api.push_message(USER_ID, TextSendMessage(text='Hello World!'))
+        line_bot_api.push_message(USER_ID, TextSendMessage(text=post_data))
     except LineBotApiError as e:
         print(e)
     return "line"
-
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080)
