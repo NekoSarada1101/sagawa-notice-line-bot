00e5e5aadb076ff01c33d40f4345d877d9cda433
NekoSarada1101 <ryoyukikazu11@gmail.com>
2022-01-20 17:19:18 +0900

    cloudfunctionsに移行

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # sagawa-notice-line-bot
 佐川急便の配達日メールをLINEで通知する
-
-build test
--- app.py
+++ app.py
@@ -1,28 +1,13 @@
-from flask import Flask, request
-from linebot import LineBotApi
-from linebot.models import TextSendMessage
-from linebot.exceptions import LineBotApiError
-from settings import *
-
-app = Flask(__name__)
+# from linebot import LineBotApi
+# from linebot.models import TextSendMessage
+# from linebot.exceptions import LineBotApiError
+# from settings import *
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-
-@app.route('/line', methods=['POST'])
-def push_notice():
-    post_data = request.get_data()
-    print(post_data.decode('unicode-escape'))
-
-    line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
-    try:
-        line_bot_api.push_message(USER_ID, TextSendMessage(text=post_data))
-    except LineBotApiError as e:
-        print(e)
+def push_notice(event, context):
+    # line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+    # try:
+    #     line_bot_api.push_message(USER_ID, TextSendMessage(text=post_data))
+    # except LineBotApiError as e:
+    #     print(e)
     return "line"
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080)
